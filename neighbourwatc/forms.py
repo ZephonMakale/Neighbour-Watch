1f68f4a61312ecc56e43967551784dca0d42ab64
ZephonMakale <mzephon@gmail.com>
2021-02-01 01:06:14 +0300

    Add the Business, Neighbourhood class in the form file

--- neighbourwatc/forms.py
+++ neighbourwatc/forms.py
@@ -15,3 +15,18 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ('user', 'neighbourhood')
+
+class NeighbourHoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighbourHood
+        exclude = ('admin',)
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user', 'neighbourhood')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
