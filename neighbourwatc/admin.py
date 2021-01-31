19f13e44a3f81ca4992e7767c11bf4e53cb8d2a7
ZephonMakale <mzephon@gmail.com>
2021-02-01 00:34:47 +0300

    Modify the admin file

---
+++ neighbourwatc/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import NeighbourHood, Profile, Business, Post
+
+admin.site.register(NeighbourHood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
